96d85f4a9c018e9a475d0c472d46404eeb76076e
dselmer <dselmer@icstars.org>
2023-08-15 09:32:15 -0500

    added more styling to search bar container 3 dots

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,29 @@
 import { Autocomplete } from "@mui/material";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { PiSelectionBackground } from "react-icons/pi";
 function SearchBar() {
   return (
     <>
-    <div className="searchbar-flex-container">
-      <div className=" search-bar-container">
-        <h1>Search Recipes With Recipe <span>Book</span> ...</h1>
-        <TextField
-          id="standard-basic"
-          label="Search Recipes"
-          variant="filled"
-          sx={{ width: "100%", Autocomplete:'disabled' }} 
-        />
-       
+      <div className="searchbar-flex-container">
+        <div className=" search-bar-container">
+          <h1>
+            Search Recipes With Recipe <span>Book</span> ...
+          </h1>
+          <div className="circle-dots-flex-container">
+            <div className="circle-dots"></div>
+            <div className="circle-dots"></div>
+            <div className="circle-dots"></div>
+          </div>
+          <TextField
+            id="standard-basic"
+            label="Search Recipes"
+            variant="filled"
+            sx={{ width: "100%", Autocomplete: "disabled" }}
+          />
+        </div>
       </div>
-    </div>
-    
-   
-    
-
-
     </>
   );
 }
