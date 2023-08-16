dfd93c7f65a5e54414071163ce1f5d1f71d36550
dselmer <dselmer@icstars.org>
2023-08-16 15:04:25 -0500

    added intialstate and min hight to recipe h3 label hight.

--- src/reducers/reccipeBookSlice.js
+++ src/reducers/reccipeBookSlice.js
@@ -3,9 +3,34 @@ import { createSlice } from '@reduxjs/toolkit'
 export const recipeBookSlice = createSlice({
   name:"recipeBook",
   initialState: {
-    mainIngredient:'tofu',
+    recpieList :[
+      {
+        mainIngredient: 'jalapeno',
+        id:null,
+        nameOfDish:'jalapeno poppers',
+        isFavorite: false,
+        img:'../../public/assests/air-fryer-jalapeno-poppers-3-1200x800-1-1024x683.jpg'
+
+      },
+      {
+        mainIngredient: 'Pork',
+        id:null,
+        nameOfDish:'Pork Belly',
+        isFavorite: false,
+        img:'../../public/assests/Pork _Belly_ramen.jpeg'
+
+      },
+      {
+        mainIngredient: 'chicken',
+        id:null,
+        nameOfDish:'Jerk Chicken',
+        isFavorite: false,
+        img:'../../public/assests/jerk_chicken.jpeg'
+      }
+    ]
   },
   reducers: {
+   
     
   }
 })
@@ -13,5 +38,6 @@ export const recipeBookSlice = createSlice({
 export const { increment, decrement, incrementByAmount } = recipeBookSlice.actions
 
 export const  selectMainIngredient = (state) =>{return state.mainIngredient};
+export const selectRecipeList= (state)=>{return state.recipeList};
 
 export default recipeBookSlice.reducer
